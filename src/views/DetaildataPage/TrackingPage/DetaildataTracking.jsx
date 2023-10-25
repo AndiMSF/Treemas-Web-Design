c1fe94f2d517ae7134a7d706eba0c18c59106706
unknown <andimuhammadsatriafadhil77@gmail.com>
2023-10-25 15:14:06 +0700

    menambahkan kode-kode google maps pada detail data tracking

--- src/views/DetaildataPage/TrackingPage/DetaildataTracking.jsx
+++ src/views/DetaildataPage/TrackingPage/DetaildataTracking.jsx
@@ -1,11 +1,26 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import "./tracking.css"
 import Information from "../../../components/Content/Information/Information"
 import BoxInput from "../../../components/Elements/BoxInput/BoxInput"
 import Navbar from "../../../components/Content/Navbar/Navbar"
 import Button from "../../../components/Elements/Buttons/Button"
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { useState } from "react"
 
-const DetaildataTracking = () => {
+const DetaildataTracking = (props) => {
+    const [selectedPlace, setSelectedPlace] = useState(null);
+
+    const onMarkerClick = (props, marker, e) => {
+        setSelectedPlace(props);
+    };
+
+    const onInfoWindowClose = () => {
+        setSelectedPlace(null);
+    };
+
     return <div className="tracking__container">
         <div className="content__container">
             <Navbar navbarText="Detail Data / Tracking" />
@@ -19,9 +34,22 @@ const DetaildataTracking = () => {
                     <div className="right__container__input">
                     </div>
                 </div>
+
+                {/* Google Maps */}
+                <Map google={props.google} zoom={14}>
+                    <Marker onClick={onMarkerClick} name={'Current location'} />
+
+                    <InfoWindow onClose={onInfoWindowClose}>
+                        <div>
+                        {selectedPlace && <h1>{selectedPlace.name}</h1>}
+                        </div>
+                    </InfoWindow>
+                </Map>
             <Information informationText="Tracking" showDropdown={false}/>
         </div>
     </div>
 }
 
-export default DetaildataTracking
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyByMFGn8i353SjJL_H0_hEfTmpUPx3_lC8")
+  })( DetaildataTracking);
