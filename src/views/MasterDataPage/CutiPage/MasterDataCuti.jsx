9928c37414dc007a6116c0167ad6de337ae239cd
unknown <andimuhammadsatriafadhil77@gmail.com>
2023-11-23 18:33:30 +0700

    menambahkan data table di claim cuti

--- src/views/MasterDataPage/CutiPage/MasterDataCuti.jsx
+++ src/views/MasterDataPage/CutiPage/MasterDataCuti.jsx
@@ -1,28 +1,199 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import "./cuti.css"
-import Information from "../../../components/Content/Information/Information"
 import Navbar from "../../../components/Content/Navbar/Navbar"
 import BoxInput from "../../../components/Elements/BoxInput/BoxInput"
-import Button from "../../../components/Elements/Buttons/Button"
-import { Link } from "react-router-dom"
+import { Link,useNavigate } from "react-router-dom"
+import DataTable from "react-data-table-component"
+import SortIcon from "@material-ui/icons/ArrowDownward";
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
+import { useEffect, useState } from "react"
+// SweetAlert
+import Swal from 'sweetalert2'
 
 const MasterDataCuti = () => {
-    const infoTopFields = ["ID", "Keterangan", "Jumlah", "Action"]
+    const navigate = useNavigate();
+    const [isToken, setIstoken] = useState('');
+    const [apiData, setApiData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    return <div className="cuti__container">
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('https://treemas-api-403500.et.r.appspot.com/api/master-data/cuti-view', {
+            method: 'GET', // Sesuaikan metode sesuai kebutuhan (GET, POST, dll.)
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`, // Sertakan token di sini
+            },
+          });
+            const data = await response.json();
+            if (data.status === 'Success') {
+              setApiData(data.data);
+            } else {
+              setError('Failed to fetch data');
+            }
+          } catch (error) {
+            setError(`Error fetching data: ${error.message}`);
+          } finally {
+            setIsLoading(false);
+          }
+        };
+    
+        const token = localStorage.getItem("authToken");
+        if (token) {
+          setIstoken(token);
+          fetchData(); // Panggil fungsi fetchData setelah mendapatkan token
+          console.log('Token: ' + token);
+        } else {
+          navigate("/login");
+        }
+      }, [navigate, apiData]);
+    
+      if (isLoading) {
+        return <div>Loading...</div>;
+      }
+    
+      if (error) {
+        return <div>Error: {error}</div>;
+      }
+    
+      const columns = [
+        {
+          name: "ID",
+          selector: "id",
+          sortable: true
+        },
+        {
+          name: "Keterangan",
+          selector: (row) => row.cutiDesc,
+          sortable: true
+        },
+        {
+            name: "Jumlah",
+            selector: "value",
+            sortable: true
+        },
+        {
+          name: "Action",
+          sortable: false,
+          cell: (d) => (
+            <>
+              <i
+                key={`edit-${d.cutiId}`}
+                onClick={() => handleClick(d.cutiId)}
+                className="first fas fa-pen"
+              ></i>
+              <i
+                key={`delete-${d.cutiId}`}
+                onClick={() => handleDelete(d.cutiId)}
+                className="fas fa-trash-alt"
+              ></i>
+            </>
+          )
+        }
+      ];
+    
+      const dataTable = {
+        columns,
+        data: apiData
+      };
+    
+    
+      const handleClick = (id) => {
+        console.log(`Edit button clicked for ID: ${id}`);
+      };
+    
+      const handleDelete = (id) => {
+          console.log(`Delete button clicked for ID: ${id}`);
+          // Tambahkan logika penghapusan data di sini, atau panggil API delete jika diperlukan
+          Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+          }).then(async (result) => {
+            if (result.isConfirmed) {
+              // Jika yes akan
+              const token = localStorage.getItem("authToken");
+          if (!token) {
+            console.error('Token is not available');
+            return;
+          }
+    
+          try {
+            const response = await fetch(`https://treemas-api-403500.et.r.appspot.com/api/master-data/cuti-form/delete/${id}`, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+              },
+            });
+    
+            const data = await response.json();
+            if (response.ok) {
+              // Berhasil dihapus
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+              // Refresh data setelah penghapusan jika diperlukan
+              fetchData();
+            } else {
+              // Gagal dihapus
+              Swal.fire({
+                title: "Error!",
+                text: data.message, // Tampilkan pesan error dari server
+                icon: "error"
+              });
+            }
+          } catch (error) {
+            // Error selama proses penghapusan
+            console.error('Error deleting data:', error);
+            Swal.fire({
+              title: "Error!",
+              text: "An error occurred while deleting the data.",
+              icon: "error"
+            });
+          }
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+        });
+      };
+
+    return (
+      <div className="cuti__container">
         <div className="content__container">
             <Navbar navbarText="Master Data / Cuti" />
-                <div className="input__container">
-                    <div className="left__container__input">
-                        <BoxInput placeholder="Keterangan Cuti" />
-                        <Button text="Pencarian" className="search__button" />
-                    </div>
-                    <div className="right__container__input">
-                    <Link to="/master-data/cuti-form/add" text="Tambah" className="add__button">Tambah</Link>
-                    </div>
-                </div>
-            <Information informationText="Cuti" showDropdown={false} fields={infoTopFields}/>
-        </div>
-    </div>
-}
+            <div className="table__container">
+                <DataTableExtensions {...dataTable}>
+                <DataTable
+                columns={columns}
+                data={apiData}
+                noHeader
+                defaultSortField="cutiId"
+                sortIcon={<SortIcon />}
+                defaultSortAsc={true}
+                pagination
+                highlightOnHover
+                dense
+                />
+                </DataTableExtensions>
+            </div>
 
+            </div>
+    </div>
+    )
+}
 export default MasterDataCuti
--- src/views/MasterDataPage/PermissionPage/MasterDataPermission.jsx
+++ src/views/MasterDataPage/PermissionPage/MasterDataPermission.jsx
@@ -1,28 +1,195 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import "./permission.css"
-import Information from "../../../components/Content/Information/Information"
 import Navbar from "../../../components/Content/Navbar/Navbar"
 import BoxInput from "../../../components/Elements/BoxInput/BoxInput"
-import Button from "../../../components/Elements/Buttons/Button"
-import { Link } from "react-router-dom"
+import { Link,useNavigate } from "react-router-dom"
+import DataTable from "react-data-table-component"
+import SortIcon from "@material-ui/icons/ArrowDownward";
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
+import { useEffect, useState } from "react"
+// SweetAlert
+import Swal from 'sweetalert2'
 
 const MasterDataPermission = () =>{
-    const infoTopFields = ["ID", "Nama Permission", "Action"]
+    const navigate = useNavigate();
+    const [isToken, setIstoken] = useState('');
+    const [apiData, setApiData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    return <div className="permission__container">
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('https://treemas-api-403500.et.r.appspot.com/api/master-data/permission-view', {
+            method: 'GET', // Sesuaikan metode sesuai kebutuhan (GET, POST, dll.)
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`, // Sertakan token di sini
+            },
+          });
+            const data = await response.json();
+            if (data.status === 'Success') {
+              setApiData(data.data);
+            } else {
+              setError('Failed to fetch data');
+            }
+          } catch (error) {
+            setError(`Error fetching data: ${error.message}`);
+          } finally {
+            setIsLoading(false);
+          }
+        };
+    
+        const token = localStorage.getItem("authToken");
+        if (token) {
+          setIstoken(token);
+          fetchData(); // Panggil fungsi fetchData setelah mendapatkan token
+          console.log('Token: ' + token);
+        } else {
+          navigate("/login");
+        }
+      }, [navigate, apiData]);
+    
+      if (isLoading) {
+        return <div>Loading...</div>;
+      }
+    
+      if (error) {
+        return <div>Error: {error}</div>;
+      }
+    
+      const columns = [
+        {
+          name: "ID",
+          selector: "id",
+          sortable: true
+        },
+        {
+          name: "Nama Permission",
+          selector: (row) => row.namaPermission,
+          sortable: true
+        },
+        {
+          name: "Action",
+          sortable: false,
+          cell: (d) => (
+            <>
+              <i
+                key={`edit-${d.id}`}
+                onClick={() => handleClick(d.id)}
+                className="first fas fa-pen"
+              ></i>
+              <i
+                key={`delete-${d.id}`}
+                onClick={() => handleDelete(d.id)}
+                className="fas fa-trash-alt"
+              ></i>
+            </>
+          )
+        }
+      ];
+    
+      const dataTable = {
+        columns,
+        data: apiData
+      };
+    
+    
+      const handleClick = (id) => {
+        console.log(`Edit button clicked for ID: ${id}`);
+      };
+    
+      const handleDelete = (id) => {
+          console.log(`Delete button clicked for ID: ${id}`);
+          // Tambahkan logika penghapusan data di sini, atau panggil API delete jika diperlukan
+          Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+          }).then(async (result) => {
+            if (result.isConfirmed) {
+              // Jika yes akan
+              const token = localStorage.getItem("authToken");
+          if (!token) {
+            console.error('Token is not available');
+            return;
+          }
+    
+          try {
+            const response = await fetch(`https://treemas-api-403500.et.r.appspot.com/api/master-data/permission-form/delete/${id}`, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+              },
+            });
+    
+            const data = await response.json();
+            if (response.ok) {
+              // Berhasil dihapus
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+              // Refresh data setelah penghapusan jika diperlukan
+              fetchData();
+            } else {
+              // Gagal dihapus
+              Swal.fire({
+                title: "Error!",
+                text: data.message, // Tampilkan pesan error dari server
+                icon: "error"
+              });
+            }
+          } catch (error) {
+            // Error selama proses penghapusan
+            console.error('Error deleting data:', error);
+            Swal.fire({
+              title: "Error!",
+              text: "An error occurred while deleting the data.",
+              icon: "error"
+            });
+          }
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+        });
+      };
+
+
+    return (
+        <div className="permission__container">
         <div className="content__container">
             <Navbar navbarText="Master Data / Permission" />
-                <div className="input__container">
-                    <div className="left__container__input">
-                        <BoxInput placeholder="Nama Permission" />
-                        <Button text="Pencarian" className="search__button" />
-                    </div>
-                    <div className="right__container__input">
-                    <Link to="/master-data/permission-form/add" text="Tambah" className="add__button">Tambah</Link> 
-                    </div>
-                </div>
-            <Information informationText="Permission" showDropdown={false} fields={infoTopFields}/>
-        </div>
+            <div className="table__container">
+                <DataTableExtensions {...dataTable}>
+                <DataTable
+                columns={columns}
+                data={apiData}
+                noHeader
+                defaultSortField="id"
+                sortIcon={<SortIcon />}
+                defaultSortAsc={true}
+                pagination
+                highlightOnHover
+                dense
+                />
+                </DataTableExtensions>
+            </div>
+
+            </div>
     </div>
+    )
 }
 
 export default MasterDataPermission
