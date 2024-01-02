4fe9b1450e72881cf8d1dbdd6af39aad06218018
divannisaa <divannisae@gmail.com>
2024-01-02 17:04:46 +0700

    project edit done

--- src/components/Elements/BoxInput/BoxInput.jsx
+++ src/components/Elements/BoxInput/BoxInput.jsx
@@ -5,7 +5,7 @@ const BoxInput = (props) => {
   return (
     <div>
         {/* Input Box */}
-        <FormControl type="text" placeholder={props.placeholder} value={props.value}
+        <FormControl disabled={props.project ? true : false} type="text" placeholder={props.placeholder} value={props.value}
         onChange={props.onChange}/>
     </div>  
 )
